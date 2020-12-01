1bbe2eb3a90c4c88aa49c1f4d06bd485a50ac605
Olivia Toolson <livytoolson@gmail.com>
2020-12-01 11:59:25 -0800

    find post by id

--- api/posts-router.js
+++ api/posts-router.js
@@ -18,9 +18,9 @@ router.post('/', (req, res) => {
             })
         })
 })
-router.post('/:id/comments', (req, res) => {
-    
-})
+// router.post('/:id/comments', (req, res) => {
+
+// })
 router.get('/', async (req, res) => {
     try {
         const posts = await Post.find()
@@ -28,6 +28,20 @@ router.get('/', async (req, res) => {
     } catch (error) {
         res.json(error.message)
     }
+})
+router.get('/:id', (req, res) => {
+    Post.findById(req.params.id)
+        .then(post => {
+            if (post) {
+                res.status(200).json(post)
+            } else {
+                res.status(404).json({ message: "The post with the specified ID does not exist." })
+            }
+        }) 
+        .catch(error => {
+            console.log(error)
+            res.status(500).json({ error: "The post information could not be retrieved." })
+        })
 })
 
 module.exports = router
